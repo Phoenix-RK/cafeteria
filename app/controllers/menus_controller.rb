4b6176fff644f04dfd3cad9a1910dc6eaed0d892
Phoenix-RK <kiruthyasp31@gmail.com>
2021-04-04 20:34:58 +0530

    Cart added

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -7,14 +7,19 @@ class MenusController < ApplicationController
   def update
     id = params[:id]
     menu = Menu.find(id)
+    cart_item = Cart.find_by(menu_id: menu.id)
+    if cart_item == nil
+      new_cart_item = Cart.create!(cart_id: 1, menu_id: menu.id, menu_name: menu.name, menu_price: menu.price, quantity: 1 )
+    else
+      cart_item.quantity = cart_item.quantity + 1
+      cart_item.save!
+    end
     if menu.quantities == 1
-      cart
       menu.quantities = menu.quantities-1
       menu.available = false
     elsif menu.quantities > 1
       menu.quantities = menu.quantities-1
     end
-
     menu.save!
     redirect_to menus_path
 
